270960d7ca6c8530b0df67f52ef3c7da5dba5465
rased100 <rased100@gmail.com>
2022-01-15 00:44:47 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import './App.css';
+import Appointment from "./Pages/Appointment/Appointment/Appointment";
+import Home from "./Pages/Home/Home/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/appointment">
+            <Appointment />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
